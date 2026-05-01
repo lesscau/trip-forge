977a0fbef11fa5f56860e734d9a876de44092e61
Olga <lesscau@gmail.com>
2026-05-01 20:51:37 +0300

    Add transaction repository

---
+++ src/main/java/com/lesscau/tripforge/transaction/LedgerTransactionRepository.java
@@ -0,0 +1,8 @@
+package com.lesscau.tripforge.transaction;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LedgerTransactionRepository extends JpaRepository<LedgerTransaction, UUID> {
+}
